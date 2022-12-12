5020c40ecd43b29ba3b3313d4b474876baa1b670
mosesochacha <ochachamoses443@gmail.com>
2022-12-12 06:21:10 +0300

    Complted

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ function isPalindrome(word) {
 /* 
   Add your pseudocode here
 */
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+// We can also save on a few variable declarations by using method chaining in our reverseString function:
 
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
 /*
   Add written explanation of your solution here
 */
